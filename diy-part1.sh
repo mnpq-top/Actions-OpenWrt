ddfc15e53132732cbb4774c8cf54982a29fac235
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2025-03-31 11:28:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo "src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds" >> feeds.conf.default
+#echo "src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds" >> feeds.conf.default
+#echo "src-git mtk_openwrt_feed https://github.com/Headcrabed/mtk-openwrt-feeds" >> feeds.conf.default
+echo "src-git mtk_openwrt_feed https://github.com/JiaY-shi/mtk-openwrt-feeds" >> feeds.conf.default
+
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
